d9b4357980a5ca6a401fd6aaf17c9026d37fdc39
SkOfficial01 <123543112+SkOfficial01@users.noreply.github.com>
2024-08-03 01:16:42 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -30,9 +30,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-             InlineKeyboardButton(
-                text=_["ᴜᴘᴅᴀᴛᴇs"],
-                url=f"https://t.me/SANKI_XDX"),
+            InlineKeyboardButton(text=_["ᴜᴘᴅᴀᴛᴇs"], url=f"https://t.me/SANKI_XDX" ),
         ],
           [
             InlineKeyboardButton(text=_["Sᴜᴘᴘᴏʀᴛ"], url=config.SUPPORT_CHAT),
