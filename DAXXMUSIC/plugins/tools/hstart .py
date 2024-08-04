469f5cd9148f1af1daec5105ceccac9570fd0235
SkOfficial01 <123543112+SkOfficial01@users.noreply.github.com>
2024-08-04 10:56:58 +0530

    Update hstart .py

--- DAXXMUSIC/plugins/tools/hstart .py
+++ DAXXMUSIC/plugins/tools/hstart .py
@@ -2,4 +2,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["start", "help"]))
 async def start_comm(c, m):
-    await m.reply_text("I am Working Broo")
+    await m.reply_text("Mᴜsɪᴄ ʙᴏᴛ ɪs ᴡᴏʀᴋɪɴɢ ᴅᴜᴅᴇ")
