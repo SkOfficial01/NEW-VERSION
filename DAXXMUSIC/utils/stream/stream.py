fb76349dc768d7e8930cd3abc2c16276df41935e
SkOfficial01 <123543112+SkOfficial01@users.noreply.github.com>
2024-08-10 19:01:33 +0530

    Update stream.py

--- DAXXMUSIC/utils/stream/stream.py
+++ DAXXMUSIC/utils/stream/stream.py
@@ -11,7 +11,7 @@ from DAXXMUSIC.misc import db
 from DAXXMUSIC.utils.database import add_active_video_chat, is_active_chat
 from DAXXMUSIC.utils.exceptions import AssistantErr
 from DAXXMUSIC.utils.inline import aq_markup, close_markup, stream_markup
-from DAXXMUSIC.utils.pastebin import DCBin
+from DAXXMUSIC.utils.pastebin import DAXXBin
 from DAXXMUSIC.utils.stream.queue import put_queue, put_queue_index
 from DAXXMUSIC.utils.thumbnails import get_thumb
 
