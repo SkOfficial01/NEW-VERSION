d078fbd9507736c9a81e305d57576d7b10b6f0c3
SkOfficial01 <123543112+SkOfficial01@users.noreply.github.com>
2024-08-10 18:37:10 +0530

    Update call.py

--- DAXXMUSIC/core/call.py
+++ DAXXMUSIC/core/call.py
@@ -17,9 +17,9 @@ from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQuality
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
-from DCxMUSIC import LOGGER, YouTube, app, YTB
-from DCxMUSIC.misc import db
-from DCxMUSIC.utils.database import (
+from DAXXMUSIC import LOGGER, YouTube, app, YTB
+from DAXXMUSIC.misc import db
+from DAXXMUSIC.utils.database import (
     add_active_chat,
     add_active_video_chat,
     get_lang,
