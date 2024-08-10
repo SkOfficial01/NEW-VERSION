eef30406d0f1907e82bb0d37510e4271cd40ea5d
SkOfficial01 <123543112+SkOfficial01@users.noreply.github.com>
2024-08-10 23:38:21 +0530

    Update buttons.py

--- DAXXMUSIC/utils/stuffs/buttons.py
+++ DAXXMUSIC/utils/stuffs/buttons.py
@@ -11,8 +11,8 @@ class BUTTONS(object):
     InlineKeyboardButton("ɢᴀᴍᴇs", callback_data="mplus HELP_Game"),InlineKeyboardButton("Ⓣ-ɢʀᴀᴘʜ", callback_data="mplus HELP_TG")],
     [InlineKeyboardButton("ɪᴍᴘᴏsᴛᴇʀ", callback_data="mplus HELP_Imposter"),
     InlineKeyboardButton("Tʀᴜᴛʜ-ᗪᴀʀᴇ", callback_data="mplus HELP_TD"),InlineKeyboardButton("ʜᴀsᴛᴀɢ", callback_data="mplus HELP_HT")], 
-    [InlineKeyboardButton("ᴛᴛs", callback_data="mplus HELP_TTS"),InlineKeyboardButton("S-ʙᴀɴ", callback_data="mplus HELP_SBAN"),
-    InlineKeyboardButton("ғᴜɴ", callback_data="mplus HELP_Fun"),InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_Q"),InlineKeyboardButton("ғᴇᴅ", callback_data="mplus HELP_FED")],          
+    [InlineKeyboardButton("ᴛᴛs", callback_data="mplus HELP_TTS"),
+    InlineKeyboardButton("ғᴜɴ", callback_data="mplus HELP_Fun"),InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_Q"),InlineKeyboardButton("ғᴇᴅ", callback_data="mplus HELP_FED"),InlineKeyboardButton("S-ʙᴀɴ", callback_data="mplus HELP_SBAN")],          
     [InlineKeyboardButton("<", callback_data=f"settings_back_helper"), 
     InlineKeyboardButton(">", callback_data=f"managebot123 settings_back_helper"),
     ]]
